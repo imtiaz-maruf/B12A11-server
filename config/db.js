707a5d7e44626102e1c08254eae8751498154840
imtiaz-maruf <maruf.techware@gmail.com>
2025-12-18 21:23:20 +0600

    Added config

---
+++ config/db.js
@@ -0,0 +1,16 @@
+// ===========================================
+// SERVER/config/db.js
+// ===========================================
+import mongoose from 'mongoose';
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGODB_URI);
+        console.log('MongoDB Connected Successfully');
+    } catch (error) {
+        console.error('MongoDB connection error:', error);
+        process.exit(1);
+    }
+};
+
+export default connectDB;
