85dce64a5d252e2b80dbd6f0991fd8fa69f0c802
imtiaz-maruf <maruf.techware@gmail.com>
2025-12-18 21:24:42 +0600

    added errorHandler

---
+++ middleware/errorHandler.js
@@ -0,0 +1,15 @@
+// ===========================================
+// SERVER/middleware/errorHandler.js
+// ===========================================
+export const errorHandler = (err, req, res, next) => {
+    console.error(err.stack);
+
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal Server Error';
+
+    res.status(statusCode).json({
+        success: false,
+        message,
+        ...(process.env.NODE_ENV === 'development' && { stack: err.stack })
+    });
+};
